67e232ddfc66c21bed63dbe1b943f017bbc7d8cb
Prabhat8055 <prabhatbhasme@gmail.com>
2026-05-11 13:07:10 +0530

    Added frontend project

---
+++ project_management/frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login.tsx";
+import Signup from "./pages/Signup.tsx";
+import Service from "./pages/Service.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/service" element={<Service />} />
+    </Routes>
+  </BrowserRouter>,
+);
